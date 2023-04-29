27ac7389774a97544d49c2f7dfd765af954bb55d
Mike Arace <mikearace@Mikes-Mac-mini.local>
2023-04-28 20:32:14 -0500

    daily commit

---
+++ controllers/todos.js
@@ -0,0 +1,14 @@
+const express = require('express'); //import express
+
+const router  = express.Router(); 
+
+router.get('/list', function(req, res) {
+
+}); 
+
+router.get('/get', function(req, res) {
+
+}); 
+
+
+module.exports = router;
